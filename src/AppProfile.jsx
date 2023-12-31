1085f26ca0f10f88b16d839ed5eead5cbe9134f2
haneul <tarabut@naver.com>
2023-12-31 22:12:43 +0900

    Event 처리하기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,10 +4,14 @@ import Profile from './components/Profile';
 import Avatar from './components/Avatar';
 
 function AppProfile() {
-
+  const handleClick = (event) =>{
+    console.log(event);
+    alert("버튼이 클릭됨");
+    }
   return (
     <>
-    <Avatar 
+      <button onClick={handleClick}>버튼</button>
+      <Avatar 
       image={'https://images.unsplash.com/photo-1703504260184-bd5c5b3719c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8'}
       isNew={true}
       />
